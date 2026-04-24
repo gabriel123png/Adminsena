aaa75c77aef0c727bb2877fa04eecbc0ba07da66
dan.munoz.dmn <dan.munoz.dmn@gmail.com>
2026-04-24 12:00:50 -0500

    terminardehacer

---
+++ Adminsena/database/migrations/2026_04_24_164058_create_course_teachers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_teachers', function (Blueprint $table) {
+            $table->id();
+            
+            $table->unsignedBigInteger('courses_id')->nullable()->unique();
+
+            $table->foreign('courses_id')
+                ->references('id')
+                ->on('courses')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+            
+            $table->unsignedBigInteger('teachers_id')->nullable()->unique();
+
+            $table->foreign('teachers_id')
+                ->references('id')
+                ->on('teachers')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_teachers');
+    }
+};
